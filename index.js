5948825d7089c974dbe276e31b35e53c81bf8fab
David Kim <mail.ddk@gmail.com>
2017-11-14 05:36:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+function printBadges() {
+  for (var i = 0; i < printBadges.length; i++) {
+    console.log(`Welcome ${printBadges}! You are employee #${i+1});
+  }
+  return printBadges;
+}
