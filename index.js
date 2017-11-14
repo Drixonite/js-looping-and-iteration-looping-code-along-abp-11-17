5b6e637234d02e8d63c1a886c8b76a378240f81c
David Kim <mail.ddk@gmail.com>
2017-11-14 05:38:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Code your solutions in this file
-const printBadges = [
-  
-]
+const printBadges = [`Ada`, `Brendan`, `Ali`]
 
 function printBadges() {
   for (var i = 0; i < printBadges.length; i++) {
