eaaed07dede991c9b130891b350df76005b7aed7
David Kim <mail.ddk@gmail.com>
2017-11-14 05:42:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Code your solutions in this file
-const printBadges = {}
 function printBadges() {
   for (var i = 0; i < printBadges.length; i++) {
     console.log(`Welcome ${printBadges}! You are employee #${i+1})`;
